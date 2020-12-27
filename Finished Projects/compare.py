4913ecef54acb21a743ed2966be8b589fc0a06c0
ProgramSmart <69406870+ProgramSmarter@users.noreply.github.com>
2020-12-27 10:07:36 -0800

    hhh

--- Finished Projects/compare.py
+++
@@ -1,7 +0,0 @@
-print("This is a comparing calculator. Input two numbers, and the program will tell you if your second number is greater than the first or not.")
-a = input("Type in a number: ")
-b = input("Type in another number: ")
-if b > a:
-  print("Your second number is greater than the first.")
-else:
-  print("Your second number is not greater than the first.")
